da0a6fac7d3b0da52693003f456a492496fd6d3d
Aayush Kumar Thakur <46928479+aayushkumarthakur@users.noreply.github.com>
2020-04-10 01:35:31 +0530

    shortpathwayroute.py
    maxint can't use in updated version of python so sys include maxsize library.
    dijkstra theory implementation

--- Shortest_path.py
+++ Shortest_path.py
@@ -1,20 +1,27 @@
+# source shortest path algorithm. The program is  
+# for adjacency matrix representation of the graph 
+  # Library for INT_MAX 
 import sys 
   
-class Graph(): 
+class Stats(): 
   
     def __init__(self, vertices): 
         self.V = vertices 
-        self.graph = [[0 for column in range(vertices)]  
-                    for row in range(vertices)] 
+        self.stat = [[0 for column in range(vertices)]  
+                      for row in range(vertices)] 
   
     def printSolution(self, dist): 
-        print "Vertex tDistance from Source"
+        print ("Vertex Distance from Source")
         for node in range(self.V): 
-            print node, "t", dist[node] 
+            print (node,"t",dist[node]) 
+  
+    # A utility function to find the vertex with  
+    # minimum distance value, from the set of vertices  
+    # not yet included in shortest path tree 
     def minDistance(self, dist, sptSet): 
   
         # Initilaize minimum distance for next node 
-        min = sys.maxint 
+        min = sys.maxsize 
   
         # Search not nearest vertex not in the  
         # shortest path tree 
@@ -24,31 +31,48 @@ class Graph():
                 min_index = v 
   
         return min_index 
-      def dijkstra(self, src): 
-      # the vertex in not in the shotest path tree 
-            for v in range(self.V): 
-                if self.graph[u][v] > 0 and \ 
-                     sptSet[v] == False and \ 
-                     dist[v] > dist[u] + self.graph[u][v]: 
-                    dist[v] = dist[u] + self.graph[u][v] 
   
-        self.printSolution(dist) 
+    # Funtion that implements Dijkstra's single source  
+    # shortest path algorithm for a graph represented  
+    # using adjacency matrix representation 
+    def dijkstra(self, src): 
   
-        dist = [sys.maxint] * self.V 
+        dist = [sys.maxsize] * self.V 
         dist[src] = 0
         sptSet = [False] * self.V 
   
         for cout in range(self.V): 
-        g = Graph(9) 
-    g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0], 
-        [4, 0, 8, 0, 0, 0, 0, 11, 0], 
-        [0, 8, 0, 7, 0, 4, 0, 0, 2], 
-        [0, 0, 7, 0, 9, 14, 0, 0, 0], 
-        [0, 0, 0, 9, 0, 10, 0, 0, 0], 
-        [0, 0, 4, 14, 10, 0, 2, 0, 0], 
-        [0, 0, 0, 0, 0, 2, 0, 1, 6], 
-        [8, 11, 0, 0, 0, 0, 1, 0, 7], 
-        [0, 0, 2, 0, 0, 0, 6, 7, 0] 
-        ]; 
   
-    g.dijkstra(0); 
+            # Pick the minimum distance vertex from  
+            # the set of vertices not yet processed.  
+            # u is always equal to src in first iteration 
+            u = self.minDistance(dist, sptSet) 
+  
+            # Put the minimum distance vertex in the  
+            # shotest path tree 
+            sptSet[u] = True
+  
+            # Update dist value of the adjacent vertices  
+            # of the picked vertex only if the current  
+            # distance is greater than new distance and 
+            # the vertex in not in the shotest path tree 
+            for v in range(self.V): 
+                if self.stat[u][v] > 0 and sptSet[v] == False:
+                    dist[v] > dist[u] + self.stat[u][v]
+                    dist[v] = dist[u] + self.stat[u][v] 
+                        
+        self.printSolution(dist) 
+  
+# Driver program 
+g  = Stats(9) 
+g.stat = [[0, 4, 0, 0, 0, 0, 0, 8, 0], 
+           [4, 0, 8, 0, 0, 0, 0, 11, 0], 
+           [0, 8, 0, 7, 0, 4, 0, 0, 2], 
+           [0, 0, 7, 0, 9, 14, 0, 0, 0], 
+           [0, 0, 0, 9, 0, 10, 0, 0, 0], 
+           [0, 0, 4, 14, 10, 0, 2, 0, 0], 
+           [0, 0, 0, 0, 0, 2, 0, 1, 6], 
+           [8, 11, 0, 0, 0, 0, 1, 0, 7], 
+           [0, 0, 2, 0, 0, 0, 6, 7, 0]]; 
+  
+g.dijkstra(0);
